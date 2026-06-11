123945165f61a6423f7aadd81badb477474f8b87
Kubota.kota <h320074b@gmail.com>
2026-06-11 14:55:40 +0900

    [update]ローソク足と移動平均のグラフを追加

--- ui/main_screen.py
+++ ui/main_screen.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+import plotly.graph_objects as go
 
 from data.stock_data import StockData
 
@@ -15,7 +16,7 @@ class MainScreen:
         st.title("株価変動予測アプリ")
 
         ticker = st.text_input(
-            "ティッカーシンボル",
+            "銘柄",
             value="7203.T"
         )
 
@@ -32,6 +33,19 @@ class MainScreen:
             index=3
         )
 
+        ma_period = st.selectbox(
+            "移動平均期間",
+            [
+            5,
+            25,
+            50,
+            75,
+            100,
+            200
+            ],
+            index=1  # 25日を初期値
+        )
+
         if st.button("データ取得"):
             with st.spinner("取得中..."):
                 df = self.stock_data.load_stock_data(
@@ -45,8 +59,11 @@ class MainScreen:
             st.success("取得完了")
 
             csv_data = self.stock_data.get_dataframe()
+
+            # 最新の株価5日分
             st.dataframe(csv_data.tail())
 
+            # 株価推移グラフ
             graph_data = csv_data.reset_index()
             fig = px.line(
                 graph_data,
@@ -54,8 +71,35 @@ class MainScreen:
                 y="Close",
                 title=f"{ticker} 株価推移"
             )
+
+            # ローソク足
+            fig.add_trace(
+                go.Candlestick(
+                    x=graph_data["Date"],
+                    open=graph_data["Open"],
+                    high=graph_data["High"],
+                    low=graph_data["Low"],
+                    close=graph_data["Close"],
+                    name="株価"
+                )
+            )
+
+            # 移動平均線
+            graph_data[f"MA{ma_period}"] = (
+                graph_data["Close"]
+                .rolling(window=ma_period)
+                .mean()
+            )
+
+            fig.add_trace(
+                go.Scatter(
+                    x=graph_data["Date"],
+                    y=graph_data[f"MA{ma_period}"],
+                    mode="lines",
+                    name=f"MA{ma_period}"
+                )
+            )
             st.plotly_chart(
                 fig,
                 use_container_width=True
             )
-
